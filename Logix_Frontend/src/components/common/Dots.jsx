3ab27620d1f2b3ccc03655595f3faa302645d259
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-03 15:52:03 +0530

    Litlle changes in dots.jsx for moblie view

--- Logix_Frontend/src/components/common/Dots.jsx
+++ Logix_Frontend/src/components/common/Dots.jsx
@@ -6,13 +6,13 @@ const Dotsbox = () => {
     <div className="fixed h-svh flex w-svw flex-col z-[-1]">
       <div className="relative w-full p-10 h-full flex justify-start items-start">
         <div
-          className="absolute inset-0 bg-cover bg-no-repeat h-28 w-52 top-28  left-10  motion-translate-x-loop-[6%] motion-translate-y-loop-[0%] motion-duration-[3500ms] motion-ease-linear"
+          className="absolute inset-0 bg-cover bg-no-repeat h-20 w-40  md:h-32 md:w-64 top-28  left-10  motion-translate-x-loop-[6%] motion-translate-y-loop-[0%] motion-duration-[3500ms] motion-ease-linear"
           style={{ backgroundImage: `url(${Dots})` }}
         ></div>
       </div>
       <div className="relative w-svw p-10 h-full ">
         <div
-          className="absolute inset-0 bg-cover bg-no-repeat  h-28 w-52 top-28  left-auto right-16 motion-translate-x-loop-[7%] motion-translate-y-loop-[0%] motion-duration-[3700ms] motion-ease-linear"
+          className="absolute inset-0 bg-cover bg-no-repeat h-20 w-40 md:h-32 md:w-64 top-28  left-auto right-16 motion-translate-x-loop-[7%] motion-translate-y-loop-[0%] motion-duration-[3700ms] motion-ease-linear"
           style={{ backgroundImage: `url(${Dots})` }}
         ></div>
       </div>
