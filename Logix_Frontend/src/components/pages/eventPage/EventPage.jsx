7f2baa38a9bb816c043a3573c9f1d3e9fcbf3f49
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-04 17:14:30 +0530

    SemdMail button added in contact.jsx

--- Logix_Frontend/src/components/pages/eventPage/EventPage.jsx
+++ Logix_Frontend/src/components/pages/eventPage/EventPage.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ContactUs from "./ContactUs";
 const EventPage = () => {
   return (
-    <>
+    <div className="flex flex-col justify-evenly">
+      <div className="h-[1000px] w-[500px]"></div>
       <ContactUs></ContactUs>
-    </>
+    </div>
   );
   
 }
