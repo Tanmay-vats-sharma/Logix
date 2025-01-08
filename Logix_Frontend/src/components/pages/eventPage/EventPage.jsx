90172ddadae65fccc4d5fc8f4a19277e543e9df5
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-08 22:59:04 +0530

    Past Events Gallery Component Work init

--- Logix_Frontend/src/components/pages/eventPage/EventPage.jsx
+++ Logix_Frontend/src/components/pages/eventPage/EventPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactUs from "./Contact Us/ContactUs";
+import PastEvents from "./Past Events/PastEventsGallery";
 const EventPage = () => {
   return (
     <div className="flex flex-col items-center justify-evenly">
       <div className="h-[1000px] w-[500px]"></div>
+      <PastEvents></PastEvents>
       <section id="intro" className="min-h-[100vh] max-w-[100vw]">
         <ContactUs selector={"#intro"} />
       </section>
