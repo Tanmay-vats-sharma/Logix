cbecd89b2b41e32ac66d31dad94d3396b3da7148
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-11 12:41:20 +0530

    observer added to gallery

--- Logix_Frontend/src/components/pages/eventPage/EventPage.jsx
+++ Logix_Frontend/src/components/pages/eventPage/EventPage.jsx
@@ -3,13 +3,13 @@ import ContactUs from "./Contact Us/ContactUs";
 import PastEvents from "./Past Events/PastEventsGallery";
 const EventPage = () => {
   return (
-    <div className="flex flex-col items-center justify-evenly">
+    <div className="flex flex-col items-center justify-evenly ">
       <div className="h-[1000px] w-full"></div>
-      <section id="pastEvents" className="min-h-[100vh] max-w-[100vw] ">
+      <section id="pastEvents" className="min-h-[100vh] max-w-[100vw] mt-10">
         <PastEvents selector={"#pastEvents"}></PastEvents>
       </section>
 
-      <section id="intro" className="min-h-[100vh] max-w-[100vw]">
+      <section id="intro" className="min-h-[100vh] max-w-[100vw] mt-10">
         <ContactUs selector={"#intro"} />
       </section>
     </div>
--- Logix_Frontend/src/components/pages/eventPage/Past Events/PastEventsGallery.jsx
+++ Logix_Frontend/src/components/pages/eventPage/Past Events/PastEventsGallery.jsx
@@ -1,45 +1,49 @@
 import React from "react";
 import { TextEffect } from "../../../motion-ui/TextEffect";
-const PastEvents = () => {
+import useIntersectionObserver from "../../../Layout/useIntersectionObserver";
+const PastEvents = ({selector}) => {
+  const isIntersecting = useIntersectionObserver({ selector });
   return (
     <>
-      <div className="h-[160vh] w-[100%] border-2">
-        <div className="h-[80vh] w-[90vw] lg:w-[75vw] flex flex-col justify-center items-center">
-          <div className="bg-[#333] text-purple-300 neon-purple-text py-1 lg:py-0 px-4 rounded-xl flex justify-center items-center text-xl sm:text-3xl lg:text-lg mb-6  box-border motion-scale-in-[0.05] sm:motion-scale-in-[0.06] md:motion-scale-in-[0.08] lg:motion-translate-x-in-[-3%] motion-translate-y-in-[-4%] motion-duration-[4000ms] motion-delay-[100ms] lg:motion-translate-x-0 sm:translate-x-0 ">
-            Visit our previous Events Gallery
+      {isIntersecting && (
+        <div className="h-[160vh] w-[100%] border-2">
+          <div className="h-[80vh] w-[90vw] lg:w-[75vw] flex flex-col justify-center items-center">
+            <div className="bg-[#333] text-purple-300 neon-purple-text py-1 lg:py-0 px-4 rounded-xl flex justify-center items-center text-xl sm:text-3xl lg:text-lg mb-6  box-border motion-scale-in-[0.05] sm:motion-scale-in-[0.06] md:motion-scale-in-[0.08] lg:motion-translate-x-in-[-3%] motion-translate-y-in-[-4%] motion-duration-[4000ms] motion-delay-[100ms] lg:motion-translate-x-0 sm:translate-x-0 ">
+              Visit our previous Events Gallery
+            </div>
+            <div className="text-[1.5rem] sm:text-3xl lg:text-4xl text-center mb-8 neon-neutral-text">
+              <TextEffect
+                per="char"
+                delay={0.5}
+                variants={{
+                  container: {
+                    hidden: { opacity: 0 },
+                    visible: {
+                      opacity: 1,
+                      transition: { staggerChildren: 0.05 },
+                    },
+                  },
+                  item: {
+                    hidden: { opacity: 0, rotateX: 90, y: 10 },
+                    visible: {
+                      opacity: 1,
+                      rotateX: 0,
+                      y: 0,
+                      transition: { duration: 0.3 },
+                    },
+                  },
+                }}
+              >
+                Relive the best moments from the Logix 2024 through vedios and photos in our gallery
+              </TextEffect>
+            </div>
           </div>
-          <div className="text-[1.5rem] sm:text-3xl lg:text-4xl text-center mb-8 neon-neutral-text">
-            <TextEffect
-              per="char"
-              delay={0.5}
-              variants={{
-                container: {
-                  hidden: { opacity: 0 },
-                  visible: {
-                    opacity: 1,
-                    transition: { staggerChildren: 0.05 },
-                  },
-                },
-                item: {
-                  hidden: { opacity: 0, rotateX: 90, y: 10 },
-                  visible: {
-                    opacity: 1,
-                    rotateX: 0,
-                    y: 0,
-                    transition: { duration: 0.3 },
-                  },
-                },
-              }}
-            >
-              Relive the best moments from the Logix 2024 through vedios and photos in our gallery
-            </TextEffect>
+
+          <div className="flex flex-col justify-around items-center">
+
           </div>
         </div>
-
-        <div className="flex flex-col justify-around items-center">
-
-        </div>
-      </div>
+      )}
     </>
   );
 }
