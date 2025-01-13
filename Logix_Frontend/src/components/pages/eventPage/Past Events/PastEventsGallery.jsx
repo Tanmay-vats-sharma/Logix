187619557320088ca254ba25115db2b0d3147c63
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-13 07:55:14 +0530

    Modal structure created

--- Logix_Frontend/src/components/pages/eventPage/Past Events/PastEventsGallery.jsx
+++ Logix_Frontend/src/components/pages/eventPage/Past Events/PastEventsGallery.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
 import { TextEffect } from "../../../motion-ui/TextEffect";
 import useObserver from "../../../Layout/useObserver";
+import SliderModal from "./Slidermodal";
+
 const PastEvents = () => {
-  const { ref, isVisible } = useObserver();
+  const { ref, isVisible } = useObserver({ once: false });
   const [isModelOpen, setIsModelOpen] = useState(false);
 
   const handleOpenModel = () => {
-    console.log(1);
-    
+    console.log("Opening Modal");
     setIsModelOpen(true);
-  }
+  };
 
   return (
     <section ref={ref} className="min-h-[100vh] max-w-[100vw] mt-10">
       {isVisible && (
         <div className="h-[160vh] w-[100%] border-2">
           <div className="h-[80vh] w-[90vw] lg:w-[75vw] flex flex-col justify-center items-center">
-            <div className="bg-[#333] text-purple-300 neon-purple-text py-1 lg:py-0 px-4 rounded-xl flex justify-center items-center text-xl sm:text-3xl lg:text-lg mb-6  box-border motion-scale-in-[0.05] sm:motion-scale-in-[0.06] md:motion-scale-in-[0.08] lg:motion-translate-x-in-[-3%] motion-translate-y-in-[-4%] motion-duration-[4000ms] motion-delay-[100ms] lg:motion-translate-x-0 sm:translate-x-0 ">
+            <div className="bg-[#333] text-purple-300 neon-purple-text py-1 lg:py-0 px-4 rounded-xl flex justify-center items-center text-xl sm:text-3xl lg:text-lg mb-6 box-border motion-scale-in-[0.05] sm:motion-scale-in-[0.06] md:motion-scale-in-[0.08] lg:motion-translate-x-in-[-3%] motion-translate-y-in-[-4%] motion-duration-[4000ms] motion-delay-[100ms] lg:motion-translate-x-0 sm:translate-x-0">
               Visit our previous Events Gallery
             </div>
             <div className="text-[1.5rem] sm:text-3xl lg:text-4xl text-center mb-8 neon-neutral-text">
@@ -42,20 +43,26 @@ const PastEvents = () => {
                   },
                 }}
               >
-                Relive the best moments from the Logix 2024 through vedios and photos in our gallery
+                Relive the best moments from the Logix 2024 through videos and
+                photos in our gallery
               </TextEffect>
             </div>
           </div>
 
           <div className="flex flex-col justify-around items-center h-auto w-[100%]">
-            <div onClick={handleOpenModel} className="h-[100px] w-[100px] border-2"></div>
-            <div onClick={handleOpenModel} className=""></div>
-            <div onClick={handleOpenModel} className=""></div>
+            <div
+              onClick={handleOpenModel}
+              className="h-[100px] w-[100px] border-2"
+            ></div>
+            <div onClick={handleOpenModel}></div>
+            <div onClick={handleOpenModel}></div>
           </div>
         </div>
       )}
+
+      {isModelOpen && <SliderModal ismodelOpen={isModelOpen} setIsModelOpen={setIsModelOpen}/>}
     </section>
   );
-}
+};
 
 export default PastEvents;
--- Logix_Frontend/src/components/pages/eventPage/Past Events/Slidermodal.jsx
+++ Logix_Frontend/src/components/pages/eventPage/Past Events/Slidermodal.jsx
@@ -6,9 +6,15 @@ const SliderModal = ({ ismodelOpen, setIsModelOpen }) => {
   }; 
   return (
     <>
-      
+      {ismodelOpen && (
+        <div className="fixed inset-0  bg-opacity-50 flex items-center justify-center z-50 hidden0px]  bg-black  ">
+          <div className="bg-white w-[90%] sm:w-[75%] md:w-[50%] lg:w-[40%] p-6 rounded-lg shadow-lg transform transition-all scale-95 text-black">
+            <button onClick={handelCloseModel}>close</button>
+          </div>
+        </div>
+      )}
     </>
-  )
+  );
 };
 
 export default SliderModal;
