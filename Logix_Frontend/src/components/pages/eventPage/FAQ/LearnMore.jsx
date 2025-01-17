54dd99be30dd7c35f515763813be802b7ecf2948
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-17 18:13:24 +0530

    LearnMore and Ques initiated

---
+++ Logix_Frontend/src/components/pages/eventPage/FAQ/LearnMore.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LearnMore = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default LearnMore;
---
+++ Logix_Frontend/src/components/pages/eventPage/FAQ/Ques.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Ques = () => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Ques;
