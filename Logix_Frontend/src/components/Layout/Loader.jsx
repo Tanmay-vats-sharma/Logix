5fc327260a0320b3516e2e50dbbbf65cfe8372e2
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-17 18:48:14 +0530

    little changes

--- Logix_Frontend/src/components/Layout/Loader.jsx
+++ Logix_Frontend/src/components/Layout/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
   return (
     <div className="fixed inset-0 flex flex-col justify-center items-center bg-[#0D0D0D] z-50">
       <div className="relative w-52 h-52 rounded-full flex items-center neon-purple-shadow justify-center">
-        <div className="absolute w-full h-full border-[6px] border-purple-500 neon-purple-shadow border-b-transparent border-l-transparent rounded-full animate-spin-reverse"></div>
+        <div className="absolute w-full h-full border-[6px] border-purple-500 neon-purple-shadow border-b-transparent border-l-transparent rounded-full animate-spin"></div>
 
         <img src={Logo} alt="Logo" className="w-48 h-32" />
       </div>
