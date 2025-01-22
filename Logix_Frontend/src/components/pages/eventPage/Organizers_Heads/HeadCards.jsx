3d5e411168003c970fa4d7a24794fe1a90cd946f
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-01-22 22:29:58 +0530

    Head and Headcard components initiated

---
+++ Logix_Frontend/src/components/pages/eventPage/Organizers_Heads/HeadCards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeadCrads = () => {
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
+}
+
+export default HeadCrads;
---
+++ Logix_Frontend/src/components/pages/eventPage/Organizers_Heads/Heads.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeadCrads from "./HeadCards";
+
+const Heads = () => {
+  return (
+    <>
+      <div className="">
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+      </div>
+    </>
+  );
+}
+
+export default Heads;
